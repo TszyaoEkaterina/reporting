6ed7ce0a759953b01a41cfc3aaf4eddc0b8d6f4f
Ekaterina <3022805283@qq.com>
2022-05-23 21:38:28 +0800

    version 2

--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -1,19 +1,38 @@
 package ru.netology;
 
 import com.github.javafaker.Faker;
+import lombok.Value;
 import lombok.experimental.UtilityClass;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 @UtilityClass
 public class DataGenerator {
-    private DataGenerator() {
-    }
+    LocalDate currentDate = LocalDate.now();
     
-    public static ApplicantData generateData(String locale) {
+    public static ApplicantData generateApplicantData(String locale) {
         Faker faker = new Faker(new Locale(locale));
         ApplicantData applicant = new ApplicantData(faker.address().city(),
                 faker.name().fullName(), faker.phoneNumber().phoneNumber());
         return applicant;
+    }
+    
+    public static String generateAppointmentDate1() {
+        String appointmentDate1 = currentDate.plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return appointmentDate1;
+    }
+    
+    public static String generateAppointmentDate2() {
+        String appointmentDate2 = currentDate.plusDays(7).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return appointmentDate2;
+    }
+    
+    @Value
+    public static class ApplicantData {
+        private String city;
+        private String fullName;
+        private String phone;
     }
 }
--- src/test/java/ru/netology/RepeatedAppointmentTest.java
+++ src/test/java/ru/netology/RepeatedAppointmentTest.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import static com.codeborne.selenide.Condition.ownText;
 import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
@@ -18,39 +15,37 @@ public class RepeatedAppointmentTest {
     void openWebsite() {
         open("http://localhost:9999");
     }
-    
-    @Test
-    void shouldRequireConfirmationAfterRepeatedAppointment() {
-        ApplicantData applicant = DataGenerator.generateData("ru");
-        LocalDate currentDate = LocalDate.now();
-        String appointmentDate1 = currentDate.plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        String appointmentDate2 = currentDate.plusDays(7).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        //first submission
-        $("[placeholder=Город]").setValue(applicant.getCity());
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);//clear field
-        $("[placeholder=\"Дата встречи\"]").setValue(appointmentDate1);
-        $(byName("name")).setValue(applicant.getFullName());
-        $(byName("phone")).setValue(applicant.getPhone());
-        $(".checkbox").click();
-        $(".grid-row button").click();
-        $("[data-test-id=success-notification] .notification__title").should(ownText("Успешно!"));
-        $("[data-test-id=success-notification] .notification__content").should(ownText("Встреча успешно " +
-                "запланирована на \r\n" + appointmentDate1));
-        //second submission
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);//clear field
-        $("[placeholder=\"Дата встречи\"]").setValue(appointmentDate2);
-        $(".grid-row button").click();
-        $("[data-test-id=replan-notification] .notification__title").should(ownText("Необходимо подтверждение"));
-        $("[data-test-id=replan-notification] .notification__content").should(ownText("У вас уже запланирована" +
-                " встреча на другую дату. Перепланировать?"));
-    }
+
+//    @Test
+//    void shouldRequireConfirmationAfterRepeatedAppointment() {
+//        DataGenerator.ApplicantData applicant = DataGenerator.generateApplicantData("ru");
+//        String appointmentDate1 = DataGenerator.generateAppointmentDate1();
+//        String appointmentDate2 = DataGenerator.generateAppointmentDate2();
+//        //first submission
+//        $("[placeholder=Город]").setValue(applicant.getCity());
+//        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);//clear field
+//        $("[placeholder=\"Дата встречи\"]").setValue(appointmentDate1);
+//        $(byName("name")).setValue(applicant.getFullName());
+//        $(byName("phone")).setValue(applicant.getPhone());
+//        $(".checkbox").click();
+//        $(".grid-row button").click();
+//        $("[data-test-id=success-notification] .notification__title").should(ownText("Успешно!"));
+//        $("[data-test-id=success-notification] .notification__content").should(ownText("Встреча успешно " +
+//                "запланирована на \r\n" + appointmentDate1));
+          //second submission
+//        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);//clear field
+//        $("[placeholder=\"Дата встречи\"]").setValue(appointmentDate2);
+//        $(".grid-row button").click();
+//        $("[data-test-id=replan-notification] .notification__title").should(ownText("Необходимо подтверждение"));
+//        $("[data-test-id=replan-notification] .notification__content").should(ownText("У вас уже запланирована" +
+//            " встреча на другую дату. Перепланировать?"));
+//    }
     
     @Test
     void shouldReplanAppointmentAfterRepeatedAppointmentAndConfirmation() {
-        ApplicantData applicant = DataGenerator.generateData("ru");
-        LocalDate currentDate = LocalDate.now();
-        String appointmentDate1 = currentDate.plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        String appointmentDate2 = currentDate.plusDays(7).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        DataGenerator.ApplicantData applicant = DataGenerator.generateApplicantData("ru");
+        String appointmentDate1 = DataGenerator.generateAppointmentDate1();
+        String appointmentDate2 = DataGenerator.generateAppointmentDate2();
         //first submission
         $("[placeholder=Город]").setValue(applicant.getCity());
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);//clear field
