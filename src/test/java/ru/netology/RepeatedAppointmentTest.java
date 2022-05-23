3ad0f8b612c98d1be87e53eb16f5a56d5d0619d9
Ekaterina <3022805283@qq.com>
2022-05-23 22:16:40 +0800

    repeated class deleted

--- src/test/java/ru/netology/RepeatedAppointmentTest.java
+++ src/test/java/ru/netology/RepeatedAppointmentTest.java
@@ -15,31 +15,6 @@ public class RepeatedAppointmentTest {
     void openWebsite() {
         open("http://localhost:9999");
     }
-
-//    @Test
-//    void shouldRequireConfirmationAfterRepeatedAppointment() {
-//        DataGenerator.ApplicantData applicant = DataGenerator.generateApplicantData("ru");
-//        String appointmentDate1 = DataGenerator.generateAppointmentDate1();
-//        String appointmentDate2 = DataGenerator.generateAppointmentDate2();
-//        //first submission
-//        $("[placeholder=Город]").setValue(applicant.getCity());
-//        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);//clear field
-//        $("[placeholder=\"Дата встречи\"]").setValue(appointmentDate1);
-//        $(byName("name")).setValue(applicant.getFullName());
-//        $(byName("phone")).setValue(applicant.getPhone());
-//        $(".checkbox").click();
-//        $(".grid-row button").click();
-//        $("[data-test-id=success-notification] .notification__title").should(ownText("Успешно!"));
-//        $("[data-test-id=success-notification] .notification__content").should(ownText("Встреча успешно " +
-//                "запланирована на \r\n" + appointmentDate1));
-          //second submission
-//        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);//clear field
-//        $("[placeholder=\"Дата встречи\"]").setValue(appointmentDate2);
-//        $(".grid-row button").click();
-//        $("[data-test-id=replan-notification] .notification__title").should(ownText("Необходимо подтверждение"));
-//        $("[data-test-id=replan-notification] .notification__content").should(ownText("У вас уже запланирована" +
-//            " встреча на другую дату. Перепланировать?"));
-//    }
     
     @Test
     void shouldReplanAppointmentAfterRepeatedAppointmentAndConfirmation() {
